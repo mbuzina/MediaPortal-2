ac78059b84a318d08be71978377b2dab0472b798
chefkoch <chefkoch@team-mediaportal.com>
2012-06-21 19:02:38 +0200

    added missing Constants.cs

---
+++ MediaPortal/Setup/MediaPortalBA/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace MediaPortal.InstallerUI
+{
+  public class Constants
+  {
+    public class VariableNames
+    {
+      public const string FeaturesAddLocal = "Features_AddLocal";
+
+      public const string ClientInstallDir = "ClientInstallDir";
+      public const string ClientDataDir = "ClientDataDir";
+      public const string ServerInstallDir = "ServerInstallDir";
+      public const string ServerDataDir = "ServerDataDir";
+    }
+  }
+}
