8b0bd93778af08a8d1a8d067edd5b715eacc60fb
Bavarian <Bavarian@team-mediaportal.com>
2012-07-15 12:18:00 +0200

    Adding Service functionality to Server, new Command Line Option -c to start Server as Console App

--- MediaPortal/Source/Main/MediaPortal.Server/Properties/AssemblyInfo.cs
+++ MediaPortal/Source/Main/MediaPortal.Server/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Team MediaPortal")]
 [assembly: AssemblyProduct("MediaPortal 2")]
-[assembly: AssemblyCopyright("Copyright © Team MediaPortal  2009")]
+[assembly: AssemblyCopyright("Copyright © Team MediaPortal  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
